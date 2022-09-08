97cae132bca0a41c43b9ea933cc46253a2ff5b06
Krzysztof027 <krzysztofszczerkowski1492@gmail.com>
2022-09-08 15:35:53 +0200

    Wyrazenia i instrukcje warunkowe if s

--- src/com/Klasa3P/Main.java
+++ src/com/Klasa3P/Main.java
@@ -7,13 +7,6 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Witaj w Javie");
-        //komentarz jendoliniowy
-        /*
-        komentarz wieloliniowy
-         */
-        /**
-         * javadoc
-         */
         int wylosowana =(int)(Math.random()*100+1);
         //random() losuje liczbę rzeczywistą z zakresu od <0,1)
         //rzutowanie zawezające
@@ -31,33 +24,6 @@ public class Main
         else
         {
             System.out.println("niestety nie udało się, ale ...");
-            int roznica =wylosowana>wylosowana ? wylosowana - wczytana : wczytana - wylosowana;
-            //wyrażenie warunkowe zwraca wartosc
-            //warunek ? zwracana wart jeżeli warunek jest prawdziwy: zwr wart war fałszywy
-            roznica =roznica/10;//dzielenie całkowite, bo różnica jest int
-            switch (roznica)
-            {
-                case 0:
-                    System.out.println("Było bardzo blisko");
-                    break;
-                case 1:
-                    System.out.println("Było blisko");
-                    break;
-                case 2:
-                    System.out.println("Trochę brakowało");
-                    break;
-                default:
-                    System.out.println("Spróbuj szczęścia gdzieś indziej");
-            }
-            //switch jako wyrażenie warunkowe
-            System.out.println(switch (roznica)
-                    {
-                        case 0 -> "Było bardzo blisko";
-                        case 1 -> "Było blisko";
-                        case 2 -> "Trochę brakowało";
-                        default -> "Dużo brakowało";
-                    }
-                    );
         }
     }
 }
