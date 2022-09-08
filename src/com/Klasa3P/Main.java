5823a27591b339c71717245bbdc3de360e818368
Krzysztof027 <krzysztofszczerkowski1492@gmail.com>
2022-09-08 14:51:00 +0200

    losowanie liczby

--- src/com/Klasa3P/Main.java
+++ src/com/Klasa3P/Main.java
@@ -5,5 +5,17 @@ public class Main
     public static void main(String[] args)
     {
         System.out.println("Witaj w Javie");
+        //komentarz jendoliniowy
+        /*
+        komentarz wieloliniowy
+         */
+        /**
+         * javadoc
+         */
+        int wylosowana =(int)(Math.random()*100+1);
+        //random() losuje liczbę rzeczywistą z zakresu od <0,1)
+        //rzutowanie zawezające
+        double innaLiczba =wylosowana;//konwersja rozszerzająca
+        System.out.println("Wylosowano "+wylosowana);
     }
 }
